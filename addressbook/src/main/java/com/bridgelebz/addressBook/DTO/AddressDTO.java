6e16df5707ea156413e7ba62e8d81707a0194c0f
Adarshpatel0308 <adarshpatel88899@gmail.com>
2025-03-09 18:28:21 +0530

    [Adarsh Patel] add. Use lambok library to auto generate the getters and setters for DTO

--- addressbook/src/main/java/com/bridgelebz/addressBook/DTO/AddressDTO.java
+++ addressbook/src/main/java/com/bridgelebz/addressBook/DTO/AddressDTO.java
@@ -1,6 +1,9 @@
 package com.bridgelebz.addressBook.DTO;
 
 
+import lombok.Data;
+
+@Data
 public class AddressDTO {
     private String fullName;
     private String address;
@@ -8,53 +11,4 @@ public class AddressDTO {
     private String state;
     private String zipCode;
     private String phoneNumber;
-
-    // Getters and Setters
-    public String getFullName() {
-        return fullName;
-    }
-
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    public String getZipCode() {
-        return zipCode;
-    }
-
-    public void setZipCode(String zipCode) {
-        this.zipCode = zipCode;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
 }
--- addressbook/src/main/java/com/bridgelebz/addressBook/model/Address.java
+++ addressbook/src/main/java/com/bridgelebz/addressBook/model/Address.java
@@ -1,86 +1,22 @@
 package com.bridgelebz.addressBook.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
 
 @Entity
-@Table(name = "address_book")
+@Data
 public class Address {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "full_name")
     private String fullName;
-
-    @Column(name = "address")
     private String address;
-
-    @Column(name = "city")
     private String city;
-
-    @Column(name = "state")
     private String state;
-
-    @Column(name = "zip_code")
     private String zipCode;
-
-    @Column(name = "phone_number")
     private String phoneNumber;
-
-    // Getters and Setters
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getFullName() {
-        return fullName;
-    }
-
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    public String getZipCode() {
-        return zipCode;
-    }
-
-    public void setZipCode(String zipCode) {
-        this.zipCode = zipCode;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
 }
